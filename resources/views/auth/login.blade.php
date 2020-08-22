8c9959aee9da7c2bb20d53e17ba6723f0aebcc96
friederkahl <frieder.kahl@k-tronik.digital>
2020-08-22 21:06:46 +0200

    formatting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,68 @@
 @extends('base')
 
 @section('nav')
-<div class="flex justify-center items-center bg-green-200 h-12 sm:h-12 md:h-16 lg:h-16 m-0">
-    <div class="text-black text-base sm:text-lg md:text-xl lg:text-2xl italic uppercase">Das <b>schwarze</b> Auge</div>
-</div>
+    <div class="flex justify-center items-center bg-green-200 h-12 sm:h-12 md:h-16 lg:h-16 m-0">
+        <div class="text-black text-base sm:text-lg md:text-xl lg:text-2xl italic uppercase">Das <b>schwarze</b> Auge</div>
+    </div>
 @endsection
 
 @section('body')
     <div class="bg-cover bg-top w-full h-full" style="background-image: url(images/third-eye.jpg);">
         <div class="flex flex-col w-full h-full justify-center items-center p-8">
-            <div class="w-full md:max-w-screen-sm">
-            <form class="px-4 py-10 bg-gray-100 bg-opacity-75" role="form" method="POST" action="{{ route('login') }}">
-            {{ csrf_field() }}
-    
-                <div class="{ $errors->has('email') ? ' has-error' : '' }}">
-                    <div class="control">
-                        <input id="email" type="email" class="w-full h-12 text-base sm:text-lg placeholder-black border border-gray-900 pl-4 mb-4 focus:bg-green-200" name="email" value="{{ old('email') }}"
-                            required autofocus placeholder="E-Mail Addresse">
-            
+            <div class="w-full md:max-w-screen-sm">˚
+                <form class="px-4 py-10 bg-gray-100 bg-opacity-75" role="form" method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+
+                    <div class="{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="control">
+                            <input id="email" type="email"
+                                class="w-full h-12 text-base sm:text-lg placeholder-black border border-gray-900 pl-4 mb-4 focus:bg-green-200"
+                                name="email" value="{{ old('email') }}" required autofocus placeholder="E-Mail Addresse">
+
                             @if ($errors->has('email'))
                                 <span class="help-block">
                                     <strong>{{ $errors->first('email') }}</strong>
                                 </span>
                             @endif
+                        </div>
                     </div>
-                </div>
-    
-                <div class=" {{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="label"></label>
-        
-                    <div class="">
-                        <input id="password" type="password" class="w-full h-12 text-base sm:text-lg placeholder-black border border-gray-900 pl-4 mb-8 focus:bg-green-200" name="password" required placeholder="Passwort">
-            
+
+                    <div class=" {{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="label"></label>
+
+                        <div class="">
+                            <input id="password" type="password"
+                                class="w-full h-12 text-base sm:text-lg placeholder-black border border-gray-900 pl-4 mb-8 focus:bg-green-200"
+                                name="password" required placeholder="Passwort">
+
                             @if ($errors->has('password'))
                                 <span class="help-block">
                                     <strong>{{ $errors->first('password') }}</strong>
                                 </span>
                             @endif
+                        </div>
                     </div>
-                </div>
 
-                 <div class="flex flex-row justify-center text-gray-200 text-base {{-- sm:text-lg --}} w-full">
-                    <label class="checkbox text-gray-800">
-                        <input type="checkbox" name="remember" class="mb-6" {{ old('remember') ? 'checked' : '' }}>
-                                Remember Me
-                    </label>
-                </div>
-    
-           {{--  <div class="flex justify-center w-full h-16 sm:text-xl bg-green-200 rounded hover:opacity-75"> --}}
-                <div class="{{-- control --}}">
-                    <button type="submit" class="flex justify-center items-center w-full h-12 text-base bg-green-600 hover:opacity-75 sm:text-lg ">
-                        Eintreten
-                    </button>
-                </div>
+                    <div
+                        class="flex flex-row justify-center text-gray-200 text-base {{-- sm:text-lg --}} w-full">
+                        <label class="checkbox text-gray-800">
+                            <input type="checkbox" name="remember" class="mb-6" {{ old('remember') ? 'checked' : '' }}>
+                            Remember Me
+                        </label>
+                    </div>
+
+                    {{-- <div
+                        class="flex justify-center w-full h-16 sm:text-xl bg-green-200 rounded hover:opacity-75">
+                        --}}
+                        <div class="{{-- control --}}">
+                            <button type="submit"
+                                class="flex justify-center items-center w-full h-12 text-base bg-green-600 hover:opacity-75 sm:text-lg ">
+                                Eintreten
+                            </button>
+                        </div>
                 </form>
-                <a href="{{ route('password.request') }}" class="flex justify-center items-center w-full h-12 text-base hover:underline">Passwort vergessen?</a>
+                <a href="{{ route('password.request') }}"
+                    class="flex justify-center items-center w-full h-12 text-base hover:underline">Passwort vergessen?</a>
 
                 <a href="" class="flex justify-center items-center w-full h-12 text-base hover:underline">Registrieren</a>
 
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
-<html style="height: 100%" >
+<html style="height: 100%">
+
 <head>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>
         window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
         ]); ?>
+
     </script>
     @yield('head')
     <title>LazzyDSA - @yield('title')</title>
 </head>
+
 <body style="height: 100%" class="bg-gray-900">
-<div @yield('vue-id') style="height: 100%">
+    <div @yield('vue-id') style="height: 100%">
 
         @yield('nav')
-        
-    @yield('body')
-</div>
-@include('vars')
-<script src="/js/app.js"></script>
+
+        @yield('body')
+    </div>
+    @include('vars')
+    <script src="/js/app.js"></script>
 </body>
+
 </html>
