239e937154d11e2b16279288f90835d11516e292
Sarah <sarah@Sarahs-MacBook-Pro.local>
2020-08-25 17:01:13 +0200

    edit specialtalents, home.blade

--- resources/views/character/specialtalents.blade.php
+++ resources/views/character/specialtalents.blade.php
@@ -1,38 +1,54 @@
-@if(!is_null($character->specialtalents->first()))
-    <div class="box is-pulled-left m-r-10">
-        <h4>Spezialtalente</h4>
-        @foreach($character->specialtalents as $specialtalent)
-            <div class="is-clearfix">
-                <b class="is-pulled-left">{{$specialtalent->name}}&nbsp;</b>
-                <p class="is-pulled-left">{{$specialtalent->pivot->value}}</p>
-                <p class="is-pulled-right">{{$specialtalent->pivot->data}}</p>
+<div class="flex flex-col md:flex-row bg-gray-400 px-5 py-4 mx-2 my-2 rounded">
+    <div>
+        @if (!is_null($character->specialtalents->first()))
+            <div class="flex flex-col bg-gray-200 p-10 mt-5 md:mx-4 rounded">
+                <h4 class="text-center text-lg font-medium">Spezialtalente</h4>
+                <div class="">
+                    @foreach ($character->specialtalents as $specialtalent)
+                        <div>
+                            <div class="">{{ $specialtalent->name }}&nbsp;</div>
+                            <div class="">{{ $specialtalent->pivot->value }}</div>
+                            <div class="">{{ $specialtalent->pivot->data }}</div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        @endforeach
+        @endif
     </div>
-@endif
 
-@if(!is_null($character->specialmagictalents->first()))
-    <div class="box is-pulled-left m-r-10">
-        <h4>Spezial Magietalente</h4>
-        @foreach($character->specialmagictalents as $specialmagictalent)
-            <div class="specialtalent-single">
-                <b class="is-pulled-left">{{$specialmagictalent->name}}&nbsp;</b>
-                <p class="is-pulled-left">{{$specialmagictalent->pivot->value}}</p>
-                <p class="is-pulled-right">{{$specialmagictalent->pivot->data}}</p>
+    <div>
+        @if (!is_null($character->specialmagictalents->first()))
+            <div class="flex flex-col bg-gray-200 p-10 mt-5 md:mx-4 rounded">
+                <h4 class="text-center text-lg font-medium">Spezialtalente>Spezial Magietalente</h4>
+                <div>
+                    @foreach ($character->specialmagictalents as $specialmagictalent)
+                        <div class="flex flex-row">
+                            <div class="">{{ $specialmagictalent->name }}&nbsp;</div>
+                            <div class="">{{ $specialmagictalent->pivot->value }}</div>
+                            <div class="">{{ $specialmagictalent->pivot->data }}</div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        @endforeach
-    </div>
-@endif
+        @endif
 
-@if(!is_null($character->specialfightingtalents->first()))
-    <div class="box is-pulled-left m-r-10">
-        <h4>Spezial Kampftalente</h4>
-        @foreach($character->specialfightingtalents as $specialfightingtalent)
-            <div class=" is-clearfix">
-                <b class="is-pulled-left">{{$specialfightingtalent->name}}&nbsp;</b>
-                <p class="is-pulled-left">{{$specialfightingtalent->pivot->value}}</p>
-                <p class="is-pulled-right">{{$specialfightingtalent->pivot->data}}</p>
-            </div>
-        @endforeach
     </div>
-@endif
+
+    <div>
+        @if (!is_null($character->specialfightingtalents->first()))
+            <div class="flex flex-col bg-gray-200 p-10 mt-5 md:mx-4 rounded">
+                <h4 class="text-center text-lg font-medium">Spezial Kampftalente</h4>
+                <div>
+                    @foreach ($character->specialfightingtalents as $specialfightingtalent)
+                        <div class="flex flex-row">
+                            <div class="">{{ $specialfightingtalent->name }}&nbsp;</div>
+                            <div class="">{{ $specialfightingtalent->pivot->value }}</div>
+                            <div class="">{{ $specialfightingtalent->pivot->data }}</div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+
+    </div>
+</div>
